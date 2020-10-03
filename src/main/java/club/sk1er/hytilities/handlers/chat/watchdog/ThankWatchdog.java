b84d39328623b326a03e3e2f36808919a9355c3a
ChachyDev <shalomadecoolboy@outlook.com>
2020-10-03 13:16:23 +0100

    Changes

--- src/main/java/club/sk1er/hytilities/handlers/chat/watchdog/ThankWatchdog.java
+++ src/main/java/club/sk1er/hytilities/handlers/chat/watchdog/ThankWatchdog.java
@@ -6,7 +6,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 
 public class ThankWatchdog implements ChatModule {
-    private static final String WATCHDOG_BAN_TRIGGER = "A player has been removed from your game for hacking or abuse. Thanks for reporting it!";
     private static final String WATCHDOG_ANNOUNCEMENT_TRIGGER = "[WATCHDOG ANNOUNCEMENT]";
     private static final String THANK_WATCHDOG_MESSAGE = "/achat Thanks Watchdog!";
 
@@ -14,7 +13,7 @@ public class ThankWatchdog implements ChatModule {
     public void onChatEvent(ClientChatReceivedEvent event) {
         String text = event.message.getUnformattedText();
 
-        if (text.contains(WATCHDOG_BAN_TRIGGER) || text.contains(WATCHDOG_ANNOUNCEMENT_TRIGGER)) {
+        if (text.equals(WATCHDOG_ANNOUNCEMENT_TRIGGER)) {
             Minecraft.getMinecraft().thePlayer.sendChatMessage(THANK_WATCHDOG_MESSAGE);
         }
     }
