dba92294e4a8ea153f71798b6366f6c02477216b
Befell <failedtestsubjet@gmail.com>
2020-12-16 17:25:33 -0800

    fix: locraw message sometimes doesn't get canceled change: removed limboplaycommandhelper  and added it into playcommand change: lobbychecker method to OnWorldLoad

--- src/main/java/club/sk1er/hytilities/handlers/lobby/LobbyChecker.java
+++ src/main/java/club/sk1er/hytilities/handlers/lobby/LobbyChecker.java
@@ -20,25 +20,34 @@ package club.sk1er.hytilities.handlers.lobby;
 
 import club.sk1er.hytilities.Hytilities;
 import club.sk1er.mods.core.util.MinecraftUtils;
-import net.minecraftforge.client.event.ClientChatReceivedEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.util.regex.Pattern;
 
 public class LobbyChecker {
-
     private final Pattern lobbyPattern = Pattern.compile("(.+)?lobby(.+)");
+    private int tick;
     private boolean lobbyStatus;
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void retrieveLocraw(ClientChatReceivedEvent event) {
-        if (!MinecraftUtils.isHypixel() || Hytilities.INSTANCE.getLocrawUtil().getLocrawInformation() == null) {
-            this.lobbyStatus = false;
+    @SubscribeEvent
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.START || Minecraft.getMinecraft().thePlayer == null || !MinecraftUtils.isHypixel() || this.tick >= 52 || Hytilities.INSTANCE.getLocrawUtil().getLocrawInformation() == null) {
             return;
         }
+        this.tick++;
+        if (this.tick == 50) {
+            this.lobbyStatus = this.lobbyPattern.matcher(Hytilities.INSTANCE.getLocrawUtil().getLocrawInformation().getServerId()).matches();
+            System.out.println(this.lobbyStatus);
+        }
 
-        this.lobbyStatus = this.lobbyPattern.matcher(Hytilities.INSTANCE.getLocrawUtil().getLocrawInformation().getServerId()).matches();
+    }
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        this.tick = 0;
     }
 
     public boolean playerIsInLobby() {
