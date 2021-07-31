c9b396579e3b94374cad259d9e3494ff95a8bb05
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-07-30 20:39:16 -0400

    change the "fix" to make it more accurate

--- src/main/java/club/sk1er/hytilities/handlers/chat/modules/blockers/MvpEmotesRemover.java
+++ src/main/java/club/sk1er/hytilities/handlers/chat/modules/blockers/MvpEmotesRemover.java
@@ -22,7 +22,6 @@ import club.sk1er.hytilities.config.HytilitiesConfig;
 import club.sk1er.hytilities.handlers.chat.ChatReceiveModule;
 import club.sk1er.hytilities.handlers.language.LanguageData;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import org.jetbrains.annotations.NotNull;
@@ -37,11 +36,10 @@ public class MvpEmotesRemover implements ChatReceiveModule {
         if (matcher.find(0)) {
             IChatComponent message = event.message;
             String cleaned = event.message.getFormattedText().replaceAll(
-                language.chatCleanerMvpEmotesRegex.pattern(), "");
+                language.chatCleanerMvpEmotesRegex.pattern(), "§r");
             ChatComponentText copy = (ChatComponentText) new ChatComponentText(message.getUnformattedTextForChat()).setChatStyle(message.getChatStyle());
             for (IChatComponent sibling : message.getSiblings()) {
-                if (cleaned.contains(sibling.getUnformattedTextForChat())) {
-                    cleaned = cleaned.replaceFirst(sibling.getUnformattedTextForChat(), "");
+                if (cleaned.contains(sibling.getFormattedText())) {
                     copy.appendSibling(sibling);
                 }
             }
